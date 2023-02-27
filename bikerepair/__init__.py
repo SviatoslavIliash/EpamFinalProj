a37e57485c41563dc120485f0d1ce08995fdfd50
Sviatoslav Iliash <iliashsviatoslav@gmail.com>
2023-02-27 22:12:24 +0200

    added config.ini file

--- bikerepair/__init__.py
+++ bikerepair/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import configparser
 
+# reading config file
+config = configparser.ConfigParser()
+config.read('bikerepair/config.ini')
+username = config['DEFAULT']['UserName']
+password = config['DEFAULT']['Password']
+host = config['DEFAULT']['Host']
+db_name = config['DEFAULT']['Database']
 
 # creating Flask application
 app = Flask(__name__)
 app.secret_key = 'some secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:2570947K!@localhost/bikerepair'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 
--- bikerepair/config.py
+++
@@ -1,5 +0,0 @@
-db_config = {
-        "host": "localhost",
-        "user": "root",
-        "pass": "2570947K!",
-    }
